cc659806aa84bbf3cc3b34766594b71c3f4c529f
YASH GUPTA <118384516+Yash-2907@users.noreply.github.com>
2025-05-19 02:55:17 +0530

    Update MainActivity.java

--- app/src/main/java/com/firstapp/whatsapp_ak/MainActivity.java
+++ app/src/main/java/com/firstapp/whatsapp_ak/MainActivity.java
@@ -10,6 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        //this acitivity can be used to add any preloader page for application
         Toast.makeText(SignInActivity.this,"this is the main acitivity", Toast.LENGTH_SHORT).show();
     }
 }
